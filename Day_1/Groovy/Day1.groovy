e7a1f6ff6ae96b09e664978fcc0ef0bae4f2fbb4
LuisSas <luis@makingdevs.com>
2018-09-01 14:06:33 -0500

    Finish day_1 with groovy

---
+++ Day_1/Groovy/Day1.groovy
@@ -0,0 +1,9 @@
+Integer i = 4
+Double d = 4.0
+String s = 'HackerRank '
+Integer i2 = new Scanner(System.in).nextLine()
+Double d2 = new Scanner(System.in).nextLine()
+String s2 = new Scanner(System.in).nextLine()
+println i + i2
+println d + d2
+println s + s2
